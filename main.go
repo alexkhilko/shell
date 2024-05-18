ebae2467ec07a8e23bd27399e123427e775d3953
Alex Khilko <alex.khilko@tripledotstudios.com>
2024-05-18 08:15:20 +0200

    Add support for piped commands

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"io"
 )
 
 func main() {
-	
 	for {
 		fmt.Print("sh> ")
 		reader := bufio.NewReader(os.Stdin)
@@ -20,37 +20,64 @@ func main() {
 		}
 
 		input = strings.TrimSpace(input)
-		parts := strings.Fields(input)
-		if len(parts) == 0 {
-			return
-		}
-		if parts[0] == "exit" {
-			return
-		}
-		if parts[0] == "cd" {
-			if len(parts) < 2 {
-				fmt.Println("cd: missing argument")
-				continue
-			}
-			if len(parts) > 2 {
-				fmt.Println("cd: too many arguments")
-				continue
-			}
-			path := parts[1]
-			err := os.Chdir(path)
-			if err != nil {	
-				fmt.Printf("cd: %s: %s\n", path, err)
-				continue
-			}
-		}
-		cmd := exec.Command(parts[0], parts[1:]...)
+		commands := strings.Split(input, "|")
+		// Initial input is from the standard input
+		var inputPipe io.Reader = os.Stdin
+		var cmds []*exec.Cmd
+		for i, command := range commands {
+			command = strings.TrimSpace(command)
+			parts := strings.Fields(command)
 
-		output, err := cmd.CombinedOutput()
-		if err != nil {
-			fmt.Println("Error executing command:", err)
-			continue
-		}
+			if len(parts) == 0 {
+				return
+			}
+			if parts[0] == "exit" {
+				return
+			}
+			if parts[0] == "cd" {
+				if len(parts) < 2 {
+					fmt.Println("cd: missing argument")
+					continue
+				}
+				if len(parts) > 2 {
+					fmt.Println("cd: too many arguments")
+					continue
+				}
+				path := parts[1]
+				err := os.Chdir(path)
+				if err != nil {	
+					fmt.Printf("cd: %s: %s\n", path, err)
+					continue
+				}
+			}
 
-		fmt.Print(string(output))
+			cmd := exec.Command(parts[0], parts[1:]...)
+			cmd.Stdin = inputPipe
+
+			if i < len(commands) - 1 {
+				outputPipe, err := cmd.StdoutPipe()
+				if err != nil {
+					fmt.Printf("Error creating stdout pipe: %s\n", err)
+					break
+				}
+				inputPipe = outputPipe
+			} else {
+				cmd.Stdout = os.Stdout
+			}
+
+			if err := cmd.Start(); err != nil {
+				fmt.Printf("Error starting command: %s\n", err)
+				break
+			}
+			cmds = append(cmds, cmd)
+		}
+		for _, cmd := range cmds {
+			if err := cmd.Wait(); err != nil {
+				fmt.Printf("Error waiting for command: %s, %v\n", strings.Join(cmd.Args, " "), err)
+				if exitErr, ok := err.(*exec.ExitError); ok {
+					fmt.Println("Exit status:", exitErr.ExitCode())
+				}
+			}
+		}
 	}
 }
